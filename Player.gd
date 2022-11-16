3cfedcffd699a13554e47be5e63872047345b99c
vinhtieu <kebintieu@gmail.com>
2022-11-16 19:07:05 +0100

    added pickup button

--- Player.gd
+++ Player.gd
@@ -8,6 +8,8 @@ var direction = Vector2();
 var run_speed = 100
 export var team = 'left'
 var attached_ball = null
+var ball_is_in_area = null
+var ball_just_picked_up = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -37,14 +39,22 @@ func read_input():
 	direction.y = direction.y * Arena.y_ratio
 	if(Input.is_action_pressed("ui_accept") and attached_ball != null):
 		direction= Vector2(0,0);
-		get_node("AnimatedSprite").modulate = (Color(1,1,0,1))
+		if(!ball_just_picked_up):
+			get_node("AnimatedSprite").modulate = (Color(1,1,0,1))
 	if(attached_ball != null and Input.is_action_just_released("ui_accept")):
-		var throw_direction = direction
-		throw_direction.x = abs(throw_direction.x)
-		throw_direction = throw_direction + Vector2(1.2,0)
-		throw_direction = throw_direction.normalized() 
-		throw_ball(throw_direction)
+		if(!ball_just_picked_up):
+			var throw_direction = direction
+			throw_direction.x = abs(throw_direction.x)
+			throw_direction = throw_direction + Vector2(1.2,0)
+			throw_direction = throw_direction.normalized() 
+			throw_ball(throw_direction)
+		else:
+			ball_just_picked_up = false 
 		get_node("AnimatedSprite").modulate = (Color(1,1,1,1))
+	if(Input.is_action_just_pressed("ui_accept") and ball_is_in_area !=null and attached_ball ==null):
+		attach_ball(ball_is_in_area)
+		ball_just_picked_up = true;
+
 
 	
 
@@ -55,9 +65,13 @@ func throw_ball(direction):
 func attach_ball(ball):
 	ball.attach(self)
 	attached_ball = ball
+	if(self.team == 'left'):
+		get_parent().current_player = self
 	
 
+
 func _on_ballbox_area_entered(area):
-	var ball = area.get_parent();
-	if(ball.get_name()=='Ball'):
-		attach_ball(ball)
+		var ball = area.get_parent();
+		if(ball.get_name()=='Ball'):
+			ball_is_in_area = ball
+

3ba00097d886ce58fc88d36f463f5f1b62d36096
vinhtieu <kebintieu@gmail.com>
2022-12-03 16:06:42 +0100

    added getting the closest player to look direction

--- Left.gd
+++ Left.gd
@@ -6,6 +6,8 @@ extends Node2D
 # var b = "text"
 var current_player = null
 var team_players = null
+var pass_player = null
+var min_dist = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,6 +22,43 @@ func _process(delta):
 		if(current_player.attached_ball == null):
 			switch(get_child((current_player.get_index() +1) % get_child_count()))
 	update()
+	var angles = []
+	var current_angle_to_min = rad2deg(current_player.position.angle_to_point(team_players[min_dist].global_position))
+
+	var current_look_angle
+	var direction = current_player.direction.normalized()
+	current_look_angle =  rad2deg(direction.angle())
+	if current_look_angle < 0:
+		current_look_angle = current_look_angle + 360
+
+	if (current_angle_to_min < 0):
+		current_angle_to_min = current_angle_to_min + 180
+		
+	var current_dif_to_min = rad2deg(min(abs(current_look_angle-current_angle_to_min),360-abs(current_look_angle-current_angle_to_min)))
+	for player in team_players:
+			var line_between = current_player.global_position + player.global_position 
+			if(player == current_player):
+				angles.append(1000)
+			else:	
+				var angle = rad2deg(player.global_position.angle_to_point(current_player.position))
+				if (angle < 0):
+					angle = angle + 360
+
+				angles.append((min(abs(current_look_angle-angle),360-abs(current_look_angle-angle))))
+	for i in range(0,len(angles)):
+		if(abs(angles[i])< abs(angles[min_dist]) and current_player.direction.length() > 0):
+			min_dist = i
+
+	for child in get_children():
+		child.modulate = Color(1,1,1)	
+	get_child(min_dist).modulate = Color(1,0,1)
+	print(current_angle_to_min)
+	print(angles)
+	print(current_look_angle,"is current look angle")
+	
+	
+				
+			
 
 func switch(var player):
 	current_player = player
